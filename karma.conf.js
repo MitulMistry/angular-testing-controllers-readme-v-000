6bea3e3d08bf35a8771e8284ba47867424029d9a
Mitul Mistry <mitul.mistry1@gmail.com>
2016-03-05 00:15:13 +0000

    Done.

--- karma.conf.js
+++ karma.conf.js
@@ -12,13 +12,19 @@ module.exports = function (config) {
             'tests/*.spec.js'
         ],
         exclude: [],
+        plugins: [
+          //require("karma-chrome-launcher"),
+          require("karma-phantomjs-launcher"),
+          require("karma-jasmine"),
+          require("karma-spec-reporter")
+        ],
         preprocessors: {},
         reporters: ['spec'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         singleRun: true,
         concurrency: Infinity
     })
